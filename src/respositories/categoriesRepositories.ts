1938e63229bec493f8dfacc46698f84a72fa5095
Higor <higor.allan21@gmail.com>
2021-11-28 21:51:39 -0300

    Criando a API com node

---
+++ src/respositories/categoriesRepositories.ts
@@ -0,0 +1,40 @@
+import { CategoryProps } from '../model/Category'
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoryRepositories {
+  private categories: CategoryProps[]; // declarando que esse categories que seria o banco de dados so e acessivel no CategoriRepositores
+
+  constructor() {
+    this.categories = []; // atribuindo um valor ao categories
+  }
+
+  create({ description, name}: ICreateCategoryDTO): void {
+    const category = new CategoryProps();
+
+    // usado para passar um objeto para a class faznedo com que o construtor da classe seja chamado ,para interar o uuid
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+    })
+  
+    this.categories.push(category)
+  }
+
+  list(): CategoryProps[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Boolean {
+    const category = this.categories.some(category => category.name === name)
+
+    return category
+  }
+
+}
+
+export { CategoryRepositories }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import {categoriesRoutes} from './routes/categories.routes'
 const app = express();
 
-app.get('/', (request, response)=>{
-  return response.json({message: "Hello world"} )
-})
+app.use(express.json())
+// usado para que todos as rotas de categoriesRoutes comeca com / categories
+app.use("/categories", categoriesRoutes)
+
 
 app.listen(3333, ()=> console.log("Server is running!"))
