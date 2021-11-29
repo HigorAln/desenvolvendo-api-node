66d28a4da61227705334a0d4e912330c7d612df3
Higor <higor.allan21@gmail.com>
2021-11-29 18:35:34 -0300

    Iniciando documentacao

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import { router } from './routes';
-import {categoriesRoutes} from './routes/categories.routes'
-import { specificationsRoutes } from './routes/specifications.routes';
+
+import swaggerFile from './swagger.json'
+
 const app = express();
 
 app.use(express.json())
 // usado para que todos as rotas de categoriesRoutes comeca com / categories
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 
 app.listen(3333, ()=> console.log("Server is running!"))
